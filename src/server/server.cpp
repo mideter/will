112a64a9fd7ede2674ed33a64f79a41b1a558ba7
mideter <mideter@yandex.ru>
2026-05-06 22:30:50 +0300

    Update server version from 2.0.1 to 2.0.2 in main function for release preparation.

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -5,7 +5,7 @@
 
 int main()
 try {
-	constexpr const char* version = "2.0.1";
+	constexpr const char* version = "2.0.2";
 	std::cout << "Starting Will Messenger Server v" << version << '\n';
 
 	const MessengerServer server;
