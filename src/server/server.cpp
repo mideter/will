0b3b398fa10d942e4cad3a1498b949e42cbbbbc2
mideter <mideter@yandex.ru>
2026-05-13 01:29:21 +0300

    Update server version to 2.1.2

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -5,7 +5,7 @@
 
 int main()
 try {
-	constexpr const char* version = "2.1.1";
+	constexpr const char* version = "2.1.2";
 	std::cout << "Starting Will Messenger Server v" << version << '\n';
 
 	will::MessengerServer server;
