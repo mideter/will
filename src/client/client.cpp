3e9a9c3f4ea52604cf8fadea5400851c8b586f34
mideter <mideter@yandex.ru>
2026-06-05 16:41:01 +0300

    Code style

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -8,9 +8,7 @@
 int main(int argc, char* argv[])
 try {
     const will::ClientConfigParser cli(argc, argv);
-    const will::ClientConfig& cfg = cli.client_config();
-
-    will::WillClient client(cfg);
+    will::WillClient client(cli.client_config());
 
     will::ChatSession chat_session(client);
     chat_session.run();
