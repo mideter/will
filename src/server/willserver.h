09c09949398d66a3188ac776d4e9674961af0477
mideter <mideter@yandex.ru>
2026-07-18 12:18:08 +0300

    Bump WillServer version to 5.1.0 for upcoming release.

--- src/server/willserver.h
+++ src/server/willserver.h
@@ -10,7 +10,7 @@ namespace will {
 
 class WillServer {
 public:
-    static constexpr const char* Version = "5.0.0";
+    static constexpr const char* Version = "5.1.0";
 
     explicit WillServer(ServerConfig config = {});
 
