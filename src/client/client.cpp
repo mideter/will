5b7cfc47fbfff1bb77c60de64ac71b7855b3c203
mideter <mideter@yandex.ru>
2026-08-27 19:55:16 +0300

    Simplify client main with will namespace usings

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -2,33 +2,39 @@
 #include "clientconfigparser.h"
 #include "consoleui.h"
 #include "devicetokenstore.h"
+#include "entities/device_token.h"
 #include "willclient.h"
 
 #include <string>
 
 
+using namespace will;
+using domain::DeviceToken;
+
+
 int main(int argc, char* argv[])
 {
-    will::ColorMode color = will::ColorMode::Auto;
+    ColorMode color = will::ColorMode::Auto;
+
     try {
-        const will::ClientConfigParser cli(argc, argv);
+        const ClientConfigParser cli(argc, argv);
         color = cli.client_config().color;
-        will::ConsoleUi ui(color);
+        ConsoleUi ui(color);
 
-        const will::domain::DeviceToken device_token =
-            will::DeviceTokenStore::load_or_create(cli.client_config().device_token_path);
+        const DeviceToken device_token =
+            DeviceTokenStore::load_or_create(cli.client_config().device_token_path);
 
-        will::WillClient client(cli.client_config());
+        WillClient client(cli.client_config());
         client.connect();
         client.authenticate_device(device_token.text());
 
-        will::ChatSession chat_session(client, ui);
+        ChatSession chat_session(client, ui);
         chat_session.run();
 
         return 0;
     }
     catch (const std::exception& e) {
-        will::ConsoleUi{color}.print_error(std::string("Client error: ") + e.what());
+        ConsoleUi{color}.print_error(std::string("Client error: ") + e.what());
         return 1;
     }
 }
